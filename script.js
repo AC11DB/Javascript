e7cc5563d5fad59a3a48487a4f971db3419dbe7d
AC11DB <cadizangelicamael@gmail.com>
2024-01-17 12:34:18 -0700

    changed script

---
+++ script.js
@@ -0,0 +1,22 @@
+const user = [
+  {
+  userId: 1234,
+  firstName: "Angel",
+  lastName: "Cadiz",
+  isActive: true,
+  },
+  {
+    userId: 1235,
+    firstName: "Copernicus",
+    lastName: "Sees the Stars",
+    isActive: false,
+  },
+  {
+  userId: 1236,
+  firstName: "Sally",
+  lastName: "Fields",
+  isActive: true,
+  }
+];
+
+console.log(user[1].firstName, user[2].lastName);
