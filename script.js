fce63f80dca23c67703fa3c14a1a9a1bab9a4a92
AC11DB <cadizangelicamael@gmail.com>
2024-01-17 12:47:07 -0700

    added funciton comparing numbers

--- script.js
+++ script.js
@@ -22,3 +22,15 @@ const user = [
 console.log(user[0].firstName, user[0].lastName + " Likes to play some classical piano songs");
 console.log("AND")
 console.log(user[1].firstName, user[1].lastName + " and" + " " + user[2].firstName, user[2].lastName + " made good piano scores")
+
+//functions comparing two numbers
+function compareNumbers(num1, num2) {
+  // Check if num1 is greater than num2
+  if (num1 > num2) {
+      return num1; // Return num1 if it's greater
+  } else {
+      return num2;
+  }
+function getFullName(user) {
+  // Concatenate the firstName and lastName properties of the user object
+  return user.firstName + " " + user.lastName;
